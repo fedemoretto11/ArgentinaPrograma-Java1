c8f418219f407c26723d04809c9456214d24f648
fedemoretto11 <fedemoretto94@gmail.com>
2023-04-25 20:46:00 -0300

    Correcion NifService ejercicio extra 2 guia 8

--- Guia 8 - POO Clases de Servicio/src/servicio/NifService.java
+++ Guia 8 - POO Clases de Servicio/src/servicio/NifService.java
@@ -15,96 +15,17 @@ public class NifService {
     private Scanner sc = new Scanner(System.in).useDelimiter("\n");
     
     public Nif crearNif(){   
+        char[] letras = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
         System.out.println("Ingrese el numero de DNI: ");
         long dni = sc.nextLong();
-        char letra = 'a';
         int resto = (int) dni % 23;
-        while(letra == 'a')
-        switch (resto){
-            case 0:
-                letra = 'T';
-                break;
-            case 1:
-                letra = 'R';
-                break;
-            case 2:
-                letra = 'W';
-                break;
-            case 3:
-                letra = 'A';
-                break;
-            case 4:
-                letra = 'G';
-                break;
-            case 5:
-                letra = 'M';
-                break;
-            case 6:
-                letra = 'Y';
-                break;
-            case 7:
-                letra = 'F';
-                break;
-            case 8:
-                letra = 'P';
-                break;
-            case 9:
-                letra = 'D';
-                break;
-            case 10:
-                letra = 'X';
-                break;
-            case 11:
-                letra = 'B';
-                break;
-            case 12:
-                letra = 'N';
-                break;
-            case 13:
-                letra = 'J';
-                break;
-            case 14:
-                letra = 'Z';
-                break;
-            case 15:
-                letra = 'S';
-                break;
-            case 16:
-                letra = 'Q';
-                break;
-            case 17:
-                letra = 'V';
-                break;
-            case 18:
-                letra = 'H';
-                break;
-            case 19:
-                letra = 'L';
-                break;
-            case 20:
-                letra = 'C';
-                break;
-            case 21:
-                letra = 'K';
-                break;
-            case 22:
-                letra = 'E';
-                break;
-            default:
-                System.out.println("Letra no encontrada");
-                letra = 'a';
-                break;
-        }
-        
+        char letra = letras[resto];
         return new Nif(dni,letra);
-
     }
     
     public void mostrar(Nif nif){
         System.out.println("Numero NIF: "+nif.getDni()+ "-"+nif.getLetter());
         System.out.println(nif.getDni() % 23);
     }
-
-
-    
+  
 }
